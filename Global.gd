253556f74e0f6287fed5f121f7b8c8589ad7cc6a
NazaQuintero <45689901+NazaQuintero@users.noreply.github.com>
2023-11-16 14:00:39 -0300

    flying enemy class extracted and initialize_values virtual function

--- Global.gd
+++ Global.gd
@@ -4,8 +4,5 @@ func instance_node(node, location, parent):
 	var node_instance = node.instantiate()
 	parent.add_child(node_instance)
 	node_instance.global_position = location
-	node_instance.hitpoints = 10
-	node_instance.max_speed = 100
-	node_instance.acceleration = 20
-	node_instance.friction = 0.5
+	node_instance._initialize_values()
 	return node_instance
---
+++ characters/enemy/flyingEnemy/FlyingEnemyFSM.gd
@@ -0,0 +1,45 @@
+extends FiniteStateMachine
+
+
+func _init() -> void:
+	_add_state("chase")
+	_add_state("hurt")
+	_add_state("dead")
+	_add_state("stun")
+
+
+func _ready() -> void:
+	_set_state(states.chase)
+
+
+func _state_logic(_delta: float) -> void:
+	match state:
+		states.chase:
+			parent.chase()
+			parent.move()
+
+
+func _get_transition() -> int:
+	match state:
+		states.hurt:
+			if not animation_player.is_playing():
+				return states.chase
+		states.stun:
+			if not animation_player.is_playing():
+				return states.chase
+	return -1
+
+
+func _enter_state(_previous_state: int, new_state: int) -> void:
+	match new_state:
+		states.chase:
+			animation_player.play("move")
+		states.hurt:
+			animation_player.play("hurt")
+			punch_sound.play()
+		states.dead:
+			punch_sound.play()
+			animation_player.play("dead")
+			flying_enemy_death_sound.play()
+		states.stun:
+			animation_player.play("stun")
