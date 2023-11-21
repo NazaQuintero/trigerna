722074cf4d7106418dfc6187c2f250e1307eff5e
NazaQuintero <45689901+NazaQuintero@users.noreply.github.com>
2023-11-20 21:25:16 -0300

    weapons inventory added, goblin sounds and crossbow art

--- weapons/crossbow.gd
+++ weapons/crossbow.gd
@@ -2,12 +2,14 @@ extends RangedWeapon
 
 
 const PROJECTILE_SCENE = preload("res://weapons/arrow.tscn")
+@onready var ARROW_THROWN_AUDIO = $ArrowThrown
 
 
 func _throw_projectile() -> void:
 	if enable_attack:
 		var projectile_init = PROJECTILE_SCENE.instantiate()
 		get_tree().current_scene.add_child(projectile_init)	
+		ARROW_THROWN_AUDIO.play()
 		projectile_init.launch(global_position, mouse_direction, projectile_speed)
 		enable_attack = false
 		cooldown_timer.start()
