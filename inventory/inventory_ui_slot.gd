d4c7762256f2be285becc41d833f0530d6c18c9d
NazaQuintero <45689901+NazaQuintero@users.noreply.github.com>
2023-12-06 17:15:11 -0300

    equipped weapons selected style

--- inventory/inventory_ui_slot.gd
+++ inventory/inventory_ui_slot.gd
@@ -2,6 +2,14 @@ extends Panel
 
 @onready var item_visual: Sprite2D = $CenterContainer/Panel/weapon_display
 
+@onready var selected_container = $PanelContainer
+
+@export var selected = false
+
+func _ready():
+	if (not selected):
+		selected_container.visible = false
+
 func update(item: InventoryItem):
 	if !item:
 		item_visual.visible = false
