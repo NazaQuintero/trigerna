8598b64e651aacb442222ff2b7b5807c96bc2d3a
tbucciarelli <tbucciarelli@hotmail.com>
2023-12-12 11:23:16 -0300

    fix

--- characters/enemy/boss/boss.gd
+++ characters/enemy/boss/boss.gd
@@ -3,28 +3,43 @@ class_name Boss
 
 var missile = preload("res://weapons/missile.tscn")
 @onready var animation = $AnimatedSprite2D
-@onready var boss_state = $FiniteMachineState
+@onready var boss_sm = $FiniteMachineState
 @onready var screen_size: Vector2 = get_viewport_rect().size
 var projectile_speed = 100
-
 var center_margin = 5
-var go_center : bool = true
+var go_center = true
+var enable_attack = false
 
 func _ready():
-	position = (screen_size / 2 )
+	pass
 
 func _process(delta):
-	mov_direction = Vector2.ZERO	
-	var direction = (player.position - position).normalized()
-	if direction.x < 0:
-		animation.flip_h = true
+	if go_center:
+		var distance_to_center = (screen_size / 2 - global_position).length()
+		if distance_to_center > center_margin:
+			mov_direction = (screen_size / 2 - global_position).normalized()
+		else:
+			mov_direction = Vector2.ZERO
 	else:
-		animation.flip_h = false
-
-func _on_attack_timer_timeout():
-	if is_instance_valid(player):
-		boss_state._set_state(boss_state.states.attack)
+		mov_direction = player.position - position
+		if mov_direction.x < 0:
+			animation.flip_h = true
+		else:
+			animation.flip_h = false
+			
+func _on_detection_area_body_entered(body):
+	if is_instance_valid(player) and enable_attack:
+		go_center = false
+		enable_attack = false
 		var missile_init = missile.instantiate()
 		get_tree().current_scene.add_child(missile_init)
+		boss_sm._set_state(boss_sm.states.attack)
 		missile_init.launch(global_position, (player.position - global_position).normalized(), projectile_speed)
 
+		
+func _on_attack_timer_timeout():
+	enable_attack = true
+
+func _on_detection_area_body_exited(body):
+	go_center = true
+	boss_sm._set_state(boss_sm.states.idle)
--- characters/enemy/boss/bossFSM.gd
+++ characters/enemy/boss/bossFSM.gd
@@ -1,18 +1,14 @@
 extends FiniteStateMachine
 
-@onready var laser = $"../RayCast2D"
-@onready var boss = $".."
-
 func _init() -> void:
 	_add_state("hurt")
 	_add_state("dead")
 	_add_state("idle")
 	_add_state("attack")
-	_add_state("glowing")
 
 
 func _ready() -> void:
-	_set_state(states.glowing)
+	_set_state(states.idle)
 
 
 func _state_logic(_delta: float) -> void:
@@ -23,23 +19,21 @@ func _get_transition() -> int:
 	match state:
 		states.hurt:
 			if not animation_player.is_playing():
-				return states.glowing
+				return states.idle
 		states.attack:
 			if not animation_player.is_playing():
-				return states.glowing
+				return states.idle
 	return -1
 
 
 func _enter_state(_previous_state: int, new_state: int) -> void:
 	match new_state:
 		states.idle:
-			animation_player.play("idle")
+			animation_player.play("glowing")
 		states.hurt:
 			animation_player.play("defense")
 		states.attack:
 			animation_player.play("missile_attack")
-		states.glowing:
-			animation_player.play("glowing")
 		states.dead:
 			animation_player.play("dead")
 			parent.drop_coin()
