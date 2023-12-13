3abf1ecfb7890839561804f3bae8fd8935cd472e
tbucciarelli <tbucciarelli@hotmail.com>
2023-12-13 02:23:27 -0300

    fix health bar

--- levels/health_bar.gd
+++ levels/health_bar.gd
@@ -1,10 +1,10 @@
 extends CanvasLayer
 
-@onready var player = get_parent()
+@onready var character = get_parent()
 @onready var bar = $TextureProgressBar
 
 func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
-	bar.value = boss.hitpoints
+	bar.value = character.hitpoints
